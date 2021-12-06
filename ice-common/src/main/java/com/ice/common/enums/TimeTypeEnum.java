1be7b642c26d6432129292b299773e797946d383
zjn <zhangjianan@agora.io>
2021-12-06 19:56:17 +0800

    clean show client conf code&new conf-detail

--- ice-common/src/main/java/com/ice/common/enums/TimeTypeEnum.java
+++ ice-common/src/main/java/com/ice/common/enums/TimeTypeEnum.java
@@ -5,61 +5,45 @@ import java.util.Map;
 
 /**
  * @author zjn
- * 时间类型 用于校验
- * 备注:
- * 1.测试版无视Server对节点到期进行的上下线行为(测试版节点即使过期也会一直存在，不会下线)
- * 2.测试版仅不会下线,但真实时间校验不通过依然不会执行
- * 3.即使是测试版,时间配置有误的节点依旧不会上线
+ * timetype describe
  */
 public enum TimeTypeEnum {
-/*
-   * 无时间限制
-   */
-  NONE((byte) 1),
-/*
-   * 大于开始时间
-   */
-  AFTER_START((byte) 5),
-/*
-   * 小于结束时间
-   */
-  BEFORE_END((byte) 6),
-/*
-   * 在开始时间与结束时间之内
-   */
-  BETWEEN((byte) 7),
-/*
-   * 测试版大于开始时间
-   */
-  TEST_AFTER_START((byte) 5),
-/*
-   * 测试版小于结束时间
-   */
-  TEST_BEFORE_END((byte) 6),
-/*
-   * 测试版在开始时间与结束时间之内
-   */
-  TEST_BETWEEN((byte) 7);
+    /*
+     * 无时间限制
+     */
+    NONE((byte) 1),
+    /*
+     * 大于开始时间
+     */
+    AFTER_START((byte) 5),
+    /*
+     * 小于结束时间
+     */
+    BEFORE_END((byte) 6),
+    /*
+     * 在开始时间与结束时间之内
+     */
+    BETWEEN((byte) 7);
 
-  private static final Map<Byte, TimeTypeEnum> MAP = new HashMap<>();
+    private static final Map<Byte, TimeTypeEnum> MAP = new HashMap<>();
 
-  static {
-    for (TimeTypeEnum enums : TimeTypeEnum.values()) {
-      MAP.put(enums.getType(), enums);
+    static {
+        for (TimeTypeEnum enums : TimeTypeEnum.values()) {
+            MAP.put(enums.getType(), enums);
+        }
     }
-  }
 
-  private final byte type;
+    private final byte type;
 
-  TimeTypeEnum(byte type) {
-    this.type = type;
-  }
+    TimeTypeEnum(byte type) {
+        this.type = type;
+    }
 
-  public static TimeTypeEnum getEnum(byte type) {
-    return MAP.get(type);
-  }
+    public static TimeTypeEnum getEnum(byte type) {
+        return MAP.get(type);
+    }
 
-  public byte getType() {
-    return type;
-  }
+    public byte getType() {
+        return type;
+    }
 }
--- ice-core/src/main/java/com/ice/core/handler/IceHandler.java
+++ ice-core/src/main/java/com/ice/core/handler/IceHandler.java
@@ -19,109 +19,109 @@ import java.util.Set;
 @Data
 public final class IceHandler {
 
-/*
-   * iceId
-   */
-  private long iceId;
+    /*
+     * iceId
+     */
+    private long iceId;
 
-/*
-   * 场景
-   */
-  private Set<String> scenes;
-/*
-   * 时间类型
-   *
-   * @see TimeTypeEnum
-   */
-  private TimeTypeEnum timeTypeEnum;
-/*
-   * 开始时间
-   */
-  private long start;
-/*
-   * 结束时间
-   */
-  private long end;
+    /*
+     * 场景
+     */
+    private Set<String> scenes;
+    /*
+     * 时间类型
+     *
+     * @see TimeTypeEnum
+     */
+    private TimeTypeEnum timeTypeEnum;
+    /*
+     * 开始时间
+     */
+    private long start;
+    /*
+     * 结束时间
+     */
+    private long end;
 
-/*
-   * handler的debug
-   * 控制着入参,出参与执行过程的打印
-   */
-  private byte debug;
+    /*
+     * handler的debug
+     * 控制着入参,出参与执行过程的打印
+     */
+    private byte debug;
 
-/*
-   * 执行根节点
-   */
-  private BaseNode root;
+    /*
+     * 执行根节点
+     */
+    private BaseNode root;
 
-  public void handle(IceContext cxt) {
-    if (DebugEnum.filter(DebugEnum.IN_PACK, debug)) {
-      log.info("handle id:{} in pack:{}", this.iceId, JSON.toJSONString(cxt.getPack()));
-    }
-    if (IceTimeUtils.timeEnable(timeTypeEnum, cxt.getPack().getRequestTime(), start, end)) {
-      return;
-    }
-    try {
-      if (root != null) {
-        root.process(cxt);
-        if (DebugEnum.filter(DebugEnum.PROCESS, debug)) {
-          log.info("handle id:{} process:{}", this.iceId, cxt.getProcessInfo().toString());
+    public void handle(IceContext cxt) {
+        if (DebugEnum.filter(DebugEnum.IN_PACK, debug)) {
+            log.info("handle id:{} in pack:{}", this.iceId, JSON.toJSONString(cxt.getPack()));
         }
-        if (DebugEnum.filter(DebugEnum.OUT_PACK, debug)) {
-          log.info("handle id:{} out pack:{}", this.iceId, JSON.toJSONString(cxt.getPack()));
-        } else {
-          if (DebugEnum.filter(DebugEnum.OUT_ROAM, debug)) {
-            log.info("handle id:{} out roam:{}", this.iceId, JSON.toJSONString(cxt.getPack().getRoam()));
-          }
+        if (IceTimeUtils.timeEnable(timeTypeEnum, cxt.getPack().getRequestTime(), start, end)) {
+            return;
         }
-      } else {
-        log.error("root not exist please check! iceId:{}", this.iceId);
-      }
-    } catch (NodeException ne) {
-      log.error("error occur in node iceId:{} node:{} cxt:{}", this.iceId, cxt.getCurrentId(), JSON.toJSONString(cxt), ne);
-    } catch (Exception e) {
-      log.error("error occur iceId:{} node:{} cxt:{}", this.iceId, cxt.getCurrentId(), JSON.toJSONString(cxt), e);
-    }
-  }
-
-  public Long getIceId() {
-    return this.iceId;
-  }
-
-  public long findIceId() {
-    return this.iceId;
-  }
-
-/*
-   * handler的debug枚举
-   * 控制着入参,出参与执行过程的打印
-   */
-  private enum DebugEnum {
-  /*
-     * 入参PACK 1
-     */
-    IN_PACK,
-  /*
-     * 执行过程(和节点debug一并使用) 2
-     */
-    PROCESS,
-  /*
-     * 结局ROAM 4
-     */
-    OUT_ROAM,
-  /*
-     * 结局PACK 8
-     */
-    OUT_PACK;
-
-    private final byte mask;
-
-    DebugEnum() {
-      this.mask = (byte) (1 << ordinal());
+        try {
+            if (root != null) {
+                root.process(cxt);
+                if (DebugEnum.filter(DebugEnum.PROCESS, debug)) {
+                    log.info("handle id:{} process:{}", this.iceId, cxt.getProcessInfo().toString());
+                }
+                if (DebugEnum.filter(DebugEnum.OUT_PACK, debug)) {
+                    log.info("handle id:{} out pack:{}", this.iceId, JSON.toJSONString(cxt.getPack()));
+                } else {
+                    if (DebugEnum.filter(DebugEnum.OUT_ROAM, debug)) {
+                        log.info("handle id:{} out roam:{}", this.iceId, JSON.toJSONString(cxt.getPack().getRoam()));
+                    }
+                }
+            } else {
+                log.error("root not exist please check! iceId:{}", this.iceId);
+            }
+        } catch (NodeException ne) {
+            log.error("error occur in node iceId:{} node:{} cxt:{}", this.iceId, cxt.getCurrentId(), JSON.toJSONString(cxt), ne);
+        } catch (Exception e) {
+            log.error("error occur iceId:{} node:{} cxt:{}", this.iceId, cxt.getCurrentId(), JSON.toJSONString(cxt), e);
+        }
     }
 
-    public static boolean filter(DebugEnum debugEnum, byte debug) {
-      return (debugEnum.mask & debug) != 0;
+    public Long getIceId() {
+        return this.iceId;
     }
-  }
+
+    public long findIceId() {
+        return this.iceId;
+    }
+
+    /*
+     * handler的debug枚举
+     * 控制着入参,出参与执行过程的打印
+     */
+    private enum DebugEnum {
+        /*
+         * 入参PACK 1
+         */
+        IN_PACK,
+        /*
+         * 执行过程(和节点debug一并使用) 2
+         */
+        PROCESS,
+        /*
+         * 结局ROAM 4
+         */
+        OUT_ROAM,
+        /*
+         * 结局PACK 8
+         */
+        OUT_PACK;
+
+        private final byte mask;
+
+        DebugEnum() {
+            this.mask = (byte) (1 << ordinal());
+        }
+
+        public static boolean filter(DebugEnum debugEnum, byte debug) {
+            return (debugEnum.mask & debug) != 0;
+        }
+    }
 }
